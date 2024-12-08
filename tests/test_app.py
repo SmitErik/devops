6745f47e71584267f990a04448ddf5e244522e99
$mitE <eriksmit2001@gmail.com>
2024-12-08 22:04:28 +0100

    kicserélve a port ip-re. Hupszi...

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,28 +10,25 @@ class TestApp(unittest.TestCase):
     def setUp(self):
         self.client = app.test_client()
         self.client.testing = True
-        self.ports = [5000, 5001, 5002, 5003]
+        self.ips = [20, 21, 22, 23]
         self.endpoints = ['/', '/current_time', '/fun_fact', '/metrics']
-
 
     def test_endpoints(self):
         successful_tests = 0
         failed_tests = 0
-        for port in self.ports:
+        for ip in self.ips:
             for endpoint in self.endpoints:
-                url = f'http://localhost:{port}{endpoint}'
+                url = f'http://172.100.0.{ip}:5000{endpoint}'
                 try:
                     response = requests.get(url)
                     self.assertEqual(response.status_code, 200)
-                    print(f'{port} on {endpoint} - OK')
                     successful_tests += 1
                 except requests.exceptions.RequestException as e:
-                    print(f'{port} on {endpoint} - Failed with error: {e}')
                     failed_tests += 1
+                    self.fail(f'{ip} on {endpoint} failed with error: {e}')
 
-        print(f'\nTests completed. Successful: {successful_tests}, Failed: {failed_tests}')
         self.assertGreater(successful_tests, 0, "No successful tests")
-
+        self.assertGreater(failed_tests, 0, f"Some tests failed. Total failed: {failed_tests}")
 
 if __name__ == '__main__':
     unittest.main()
