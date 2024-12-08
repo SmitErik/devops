c12b14858dc18742b7f03f95bd5c4b02710ab374
$mitE <eriksmit2001@gmail.com>
2024-12-08 21:36:47 +0100

    modified so '..' is in PATH

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'app')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import unittest
 from main import app
